8356be0842f3bb7654450f05428e2b4cc4cc249b
Will Chen <108688228+will-d-chen@users.noreply.github.com>
2024-06-25 15:38:19 -0400

    Update ur_sim_moveit.launch.py

--- ur_simulation_gazebo/launch/ur_sim_moveit.launch.py
+++ ur_simulation_gazebo/launch/ur_sim_moveit.launch.py
@@ -99,7 +99,7 @@ def generate_launch_description():
             "ur_type",
             description="Type/series of used UR robot.",
             choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
-            default_value="ur5e",
+            default_value="ur3e",
         )
     )
     declared_arguments.append(
